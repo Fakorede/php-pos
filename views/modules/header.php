527850003e13580a4debb665ba56104fac4caca9
fakorede <abiolafakorede@gmail.com>
2020-04-29 14:53:48 +0100

    header module

---
+++ views/modules/header.php
@@ -0,0 +1,55 @@
+<header class="main-header">
+    <!--==========================================
+                        LOGO
+    ===========================================-->
+    <a href="" class="logo">
+        <!-- min logo -->
+        <span class="logo-mini">
+            <img src="views/img/template/icono-blanco.png" class="img-responsive" style="padding:10px">
+        </span>
+
+        <!-- normal logo -->
+        <span class="logo-lg">
+            <img src="views/img/template/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0px">
+        </span>
+    </a>
+
+    <!--==========================================
+                        NAVBAR
+    ===========================================-->
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <!-- navigation button -->
+
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <!-- user profile -->
+
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="views/img/users/default/anonymous.png" class="user-image">
+                        <span class="hidden-xs">Admin User</span>
+                    </a>
+
+                     <!-- dropdown -->
+                    <ul class="dropdown-menu">
+                        <li class="user-body">
+                            <div class="pull-right">
+                                <a href="#" class="btn btn-default btn-flat">Log out</a>
+                            </div>
+                        </li>
+                    </ul>
+
+                </li>
+            </ul>
+        </div>
+
+
+    </nav>
+
+
+</header>
