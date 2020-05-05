e754482c292de6e4364179d20aa7e47cdf4cde2e
fakorede <abiolafakorede@gmail.com>
2020-05-05 21:29:59 +0100

    Manage User Page

--- views/modules/users.php
+++ views/modules/users.php
@@ -7,40 +7,89 @@
     </h1>
 
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
       <li class="active">Manage Users</li>
     </ol>
-    
+
   </section>
 
   <!-- Main content -->
   <section class="content">
 
-    <!-- Default box -->
     <div class="box">
-      <div class="box-header with-border">
-        <h3 class="box-title">Title</h3>
 
-        <div class="box-tools pull-right">
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                  title="Collapse">
-            <i class="fa fa-minus"></i></button>
-          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fa fa-times"></i></button>
-        </div>
+      <div class="box-header with-border">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#addUserModal">
+          Add User
+        </button>
       </div>
+
       <div class="box-body">
-        Start creating your amazing application!
+        <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Picture</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Last Login</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Admin User</td>
+              <td>Admin</td>
+              <td><img src="views/img/users/default/anonymous.png" width="40px" class="img-thumbnail"></td>
+              <td>Administrator</td>
+              <td><button class="btn btn-success">Activated</button></td>
+              <td>2020-05-05 21:15:50</td>
+              <td>
+                <div class="btn-group">
+                  <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
       <!-- /.box-body -->
-      <div class="box-footer">
-        Footer
-      </div>
-      <!-- /.box-footer-->
+
     </div>
-    <!-- /.box -->
 
   </section>
-  <!-- /.content -->
+
 </div>
-<!-- /.content-wrapper -->
+
+<!-- ============================================
+ADD USER MODAL
+============================================= -->
+
+<div class="modal fade" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="addUserModalLabel" aria-hidden="true">
+
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+
+      <div class="modal-body">
+        <p>Some text</p>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+
+  </div>
+</div>
